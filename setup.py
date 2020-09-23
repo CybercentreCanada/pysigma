5201eec6289d2f5ae1bf9315c27ab775c1ec3968
Benjamin Stix <benjaminstix@outlook.com>
2020-09-23 13:20:30 -0400

    moved library files into a python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sigma-signature",
+    version="0.0.1",
+    description="A library for parsing sysmon logs against sigma rules",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages()
+)
