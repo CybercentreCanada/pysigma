6badff11ef0b78fac818c31b6b49e931d6f5bf4f
Benjamin Stix <benjaminstix@outlook.com>
2020-10-07 10:34:53 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sigma-signature",
-    version="0.0.1",
+    version="0.0.2",
     description="A library for parsing sysmon logs against sigma rules",
     long_description=long_description,
     long_description_content_type="text/markdown",
