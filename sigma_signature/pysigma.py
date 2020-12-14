15c9ca1b9436543b183ff30a4104992f46324182
Benjamin Stix <benjaminstix@outlook.com>
2020-12-14 14:57:15 -0500

    error if there's no detection element

--- sigma_signature/pysigma.py
+++ sigma_signature/pysigma.py
@@ -11,7 +11,9 @@ class PySigma:  # what should I name it?
 
     def add_signature(self, signature_file):
         name, signature = signatures.loadSignature(signature_file)
-        detection = signature['detection']
+        detection = signature.get('detection')
+        if not detection:
+            raise ValueError("No detection key in signature")
         if 'near' in detection['condition']:
             raise ValueError("near-aggregation is not supported")
         self.rules[name] = signature
